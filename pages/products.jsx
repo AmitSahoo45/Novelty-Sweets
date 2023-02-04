2f481e31d7cc69d9653b711bc5ec38803f441426
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-02-04 23:31:09 +0530

    Removed Error

--- pages/products.jsx
+++ pages/products.jsx
@@ -75,18 +75,4 @@ const products = () => {
     )
 }
 
-// export async function getServerSideProps(context) {
-//     let pdt = await fetch(`http://localhost:1337/api/sweets?populate=*`,
-//         {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Authorization: 'Bearer 927507f30f322bad130dff6e252af9c0991e2cf29ccd6428304da89b2e86c711122bf42933082edb0c1841b739491a01610c1a2ce35d3b3a2e4d8be2ec009acce5190c6f181a4a6489058f592a5f315cbd2726ade4b58a1902cd149f3f3b602a3cf015fd4455e7bc568f965496cc45974297c3be990beafba8ac225160687908'
-//             }
-//         })
-//     let products = await pdt.json()
-
-//     return { props: { products } }
-// }
-
 export default products
